bbfd5504c97f2e64343816bd027ddbcd4a39d32f
Harsh Agarwal <harshagarwa@adobe.com>
2025-05-30 11:48:45 +0530

    update

--- monitor.h
+++ monitor.h
@@ -1,6 +1,3 @@
 #pragma once
 
-bool isTemperatureOk(float temperature);
-bool isPulseRateOk(float pulseRate);
-bool isSpo2Ok(float spo2);
 int vitalsOk(float temperature, float pulseRate, float spo2);
--- test-monitor.cpp
+++ test-monitor.cpp
@@ -6,12 +6,4 @@ TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
 }
 
-TEST(Monitor, EdgeCases) {
-  ASSERT_FALSE(vitalsOk(94.9, 70, 98)); // Temperature just below range
-  ASSERT_FALSE(vitalsOk(102.1, 70, 98)); // Temperature just above range
-  ASSERT_FALSE(vitalsOk(98.1, 59.9, 98)); // Pulse rate just below range
-  ASSERT_FALSE(vitalsOk(98.1, 100.1, 98)); // Pulse rate just above range
-  ASSERT_FALSE(vitalsOk(98.1, 70, 89.9)); // Spo2 just below range
-  ASSERT_TRUE(vitalsOk(95, 60, 90)); // Lower bounds of all vitals
-  ASSERT_TRUE(vitalsOk(102, 100, 90)); // Upper bounds of all vitals
-}
+
